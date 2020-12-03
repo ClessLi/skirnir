490547c7d4ef3048104ae003091eb691aabe0ca1
ClessLi <316600949@qq.com>
2020-12-03 17:06:44 +0800

    全改用go-kit框架

--- pkg/discover/discover_client.go
+++ pkg/discover/discover_client.go
@@ -1,9 +1,12 @@
 // discover 包，该包包含了服务发现注册接口定义，与基于go-kit(gRPC协议)框架的consul服务注册发现客户端结构体
 package discover
 
-import "log"
+import (
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/sd"
+)
 
-type DiscoveryClient interface {
+type RegistryClient interface {
 
 	/*服务注册接口
 	  @param serviceName 服务名
@@ -12,13 +15,16 @@ type DiscoveryClient interface {
 	  @param healthCheckUrl 健康检查地址
 	  @param instanceHost 服务实例地址
 	  @param meta 服务实例元数据*/
-	Register(serviceName, instanceId, instanceHost string, instancePort uint16, meta map[string]string, logger *log.Logger) bool
+	Register(serviceName, instanceId, instanceHost string, instancePort uint16, meta map[string]string, logger log.Logger) bool
 
 	/*服务注销接口
 	  @param instanceId 服务实例Id*/
-	DeRegister(instanceId string, logger *log.Logger) bool
+	DeRegister(instanceId string, logger log.Logger) bool
+}
+
+type DiscoveryClient interface {
 
 	/*发现服务实例接口
 	  @param serviceName 服务名*/
-	DiscoverServices(serviceName string, logger *log.Logger) []interface{}
+	DiscoverServices(serviceName string, logger log.Logger) sd.Instancer
 }
--- pkg/discover/kit_consul_client.go
+++ pkg/discover/kit_consul_client.go
@@ -2,10 +2,11 @@ package discover
 
 import (
 	"fmt"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/sd"
 	"github.com/go-kit/kit/sd/consul"
 	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/consul/api/watch"
-	"log"
 	"strconv"
 	"sync"
 )
@@ -21,7 +22,7 @@ type KitConsulClient struct {
 	instancesMap sync.Map
 }
 
-func NewKitConsulClient(consulHost string, consulPort uint16) (DiscoveryClient, error) {
+func NewKitConsulRegistryClient(consulHost string, consulPort uint16) (RegistryClient, error) {
 	// 通过 Consul Host 和 Consul Port 创建一个 consul.Client
 	consulConfig := api.DefaultConfig()
 	consulConfig.Address = consulHost + ":" + strconv.Itoa(int(consulPort))
@@ -38,7 +39,24 @@ func NewKitConsulClient(consulHost string, consulPort uint16) (DiscoveryClient, 
 	}, err
 }
 
-func (consulClient *KitConsulClient) Register(serviceName, instanceId, instanceHost string, instancePort uint16, meta map[string]string, logger *log.Logger) bool {
+func NewKitConsulDiscoveryClient(consulHost string, consulPort uint16) (DiscoveryClient, error) {
+	// 通过 Consul Host 和 Consul Port 创建一个 consul.Client
+	consulConfig := api.DefaultConfig()
+	consulConfig.Address = consulHost + ":" + strconv.Itoa(int(consulPort))
+	apiClient, err := api.NewClient(consulConfig)
+	if err != nil {
+		return nil, err
+	}
+	client := consul.NewClient(apiClient)
+	return &KitConsulClient{
+		Host:   consulHost,
+		Port:   consulPort,
+		config: consulConfig,
+		client: client,
+	}, err
+}
+
+func (consulClient *KitConsulClient) Register(serviceName, instanceId, instanceHost string, instancePort uint16, meta map[string]string, logger log.Logger) bool {
 	serviceRegistration := &api.AgentServiceRegistration{
 		ID:      instanceId,
 		Name:    serviceName,
@@ -47,21 +65,21 @@ func (consulClient *KitConsulClient) Register(serviceName, instanceId, instanceH
 		Meta:    meta,
 		Check: &api.AgentServiceCheck{
 			Interval:                       "15s",
-			GRPC:                           fmt.Sprintf("%v:%v/grpc.health.v1.%v", instanceHost, instancePort, serviceName),
+			GRPC:                           fmt.Sprintf("%v:%v/%v", instanceHost, instancePort, serviceName),
 			DeregisterCriticalServiceAfter: "30s",
 		},
 	}
 	err := consulClient.client.Register(serviceRegistration)
 
 	if err != nil {
-		log.Printf("Register Service Error! Cased by: %s\n", err)
+		logger.Log(fmt.Sprintf("Register Service Error! Cased by: %s\n", err))
 		return false
 	}
-	log.Println("Register Service Success!")
+	logger.Log("Register Service Success!")
 	return true
 }
 
-func (consulClient *KitConsulClient) DeRegister(instanceId string, logger *log.Logger) bool {
+func (consulClient *KitConsulClient) DeRegister(instanceId string, logger log.Logger) bool {
 
 	// 构建包含服务实例 ID 的元数据结构体
 	serviceRegistration := &api.AgentServiceRegistration{
@@ -71,72 +89,73 @@ func (consulClient *KitConsulClient) DeRegister(instanceId string, logger *log.L
 	err := consulClient.client.Deregister(serviceRegistration)
 
 	if err != nil {
-		logger.Printf("Deregister Service Error! Cased by: %s\n", err)
+		logger.Log(fmt.Sprintf("Deregister Service Error! Cased by: %s\n", err))
 		return false
 	}
-	log.Println("Deregister Service Success!")
+	logger.Log("Deregister Service Success!")
 
 	return true
 }
 
-func (consulClient *KitConsulClient) DiscoverServices(serviceName string, logger *log.Logger) []interface{} {
+func (consulClient *KitConsulClient) DiscoverServices(serviceName string, logger log.Logger) sd.Instancer {
 
-	//  该服务已监控并缓存
-	instanceList, ok := consulClient.instancesMap.Load(serviceName)
-	if ok {
-		return instanceList.([]interface{})
-	}
-	// 申请锁
-	consulClient.mutex.Lock()
-	// 再次检查是否监控
-	instanceList, ok = consulClient.instancesMap.Load(serviceName)
-	if ok {
-		return instanceList.([]interface{})
-	} else {
-		// 注册监控
-		go func() {
-			// 使用 consul 服务实例监控来监控某个服务名的服务实例列表变化
-			params := make(map[string]interface{})
-			params["type"] = "service"
-			params["service"] = serviceName
-			plan, _ := watch.Parse(params)
-			plan.Handler = func(u uint64, i interface{}) {
-				if i == nil {
-					return
-				}
-				v, ok := i.([]*api.ServiceEntry)
-				if !ok {
-					return // 数据异常，忽略
-				}
-				// 没有服务实例在线
-				if len(v) == 0 {
-					consulClient.instancesMap.Store(serviceName, []interface{}{})
-				}
-				var healthServices []interface{}
-				for _, service := range v {
-					if service.Checks.AggregatedStatus() == api.HealthPassing {
-						healthServices = append(healthServices, service.Service)
-					}
-				}
-				consulClient.instancesMap.Store(serviceName, healthServices)
-			}
-			defer plan.Stop()
-			plan.Run(consulClient.config.Address)
-		}()
-	}
-	defer consulClient.mutex.Unlock()
-
-	// 根据服务名请求服务实例列表
-	entries, _, err := consulClient.client.Service(serviceName, "", false, nil)
-	if err != nil {
-		consulClient.instancesMap.Store(serviceName, []interface{}{})
-		logger.Printf("Discover Service Error! Cased by: %s\n", err)
-		return nil
-	}
-	instances := make([]interface{}, len(entries))
-	for i := 0; i < len(instances); i++ {
-		instances[i] = entries[i].Service
-	}
-	consulClient.instancesMap.Store(serviceName, instances)
-	return instances
+	return consul.NewInstancer(consulClient.client, logger, serviceName, nil, true)
+	////  该服务已监控并缓存
+	//instanceList, ok := consulClient.instancesMap.Load(serviceName)
+	//if ok {
+	//	return instanceList.([]interface{})
+	//}
+	//// 申请锁
+	//consulClient.mutex.Lock()
+	//// 再次检查是否监控
+	//instanceList, ok = consulClient.instancesMap.Load(serviceName)
+	//if ok {
+	//	return instanceList.([]interface{})
+	//} else {
+	//	// 注册监控
+	//	go func() {
+	//		// 使用 consul 服务实例监控来监控某个服务名的服务实例列表变化
+	//		params := make(map[string]interface{})
+	//		params["type"] = "service"
+	//		params["service"] = serviceName
+	//		plan, _ := watch.Parse(params)
+	//		plan.Handler = func(u uint64, i interface{}) {
+	//			if i == nil {
+	//				return
+	//			}
+	//			v, ok := i.([]*api.ServiceEntry)
+	//			if !ok {
+	//				return // 数据异常，忽略
+	//			}
+	//			// 没有服务实例在线
+	//			if len(v) == 0 {
+	//				consulClient.instancesMap.Store(serviceName, []interface{}{})
+	//			}
+	//			var healthServices []interface{}
+	//			for _, service := range v {
+	//				if service.Checks.AggregatedStatus() == api.HealthPassing {
+	//					healthServices = append(healthServices, service.Service)
+	//				}
+	//			}
+	//			consulClient.instancesMap.Store(serviceName, healthServices)
+	//		}
+	//		defer plan.Stop()
+	//		plan.Run(consulClient.config.Address)
+	//	}()
+	//}
+	//defer consulClient.mutex.Unlock()
+	//
+	//// 根据服务名请求服务实例列表
+	//entries, _, err := consulClient.client.Service(serviceName, "", false, nil)
+	//if err != nil {
+	//	consulClient.instancesMap.Store(serviceName, []interface{}{})
+	//	logger.Log(fmt.Sprintf("Discover Service Error! Cased by: %s\n", err))
+	//	return nil
+	//}
+	//instances := make([]interface{}, len(entries))
+	//for i := 0; i < len(instances); i++ {
+	//	instances[i] = entries[i].Service
+	//}
+	//consulClient.instancesMap.Store(serviceName, instances)
+	//return instances
 }
